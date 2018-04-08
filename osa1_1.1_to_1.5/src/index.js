cf7a094710849e2afaeecdcd8273c06ef9c84b3f
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-04-08 15:31:52 +0300

    Ignore .vscode, fix whitespace
    Since the course seems to use 2 spaces in its examples,
    let's also use 2 spaces instead of 4.
    
    Also, add .vscode to .gitignore.

--- osa1_1.1_to_1.5/src/index.js
+++ osa1_1.1_to_1.5/src/index.js
@@ -2,58 +2,59 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Otsikko = (props) => {
-    return (
-        <div>
-            <h1>{props.kurssi}</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{props.kurssi}</h1>
+    </div>
+  )
 }
 
 const Osa = (props) => {
-    return (
-        <div>
-            <p>{props.nimi} {props.tehtavia}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>{props.nimi} {props.tehtavia}</p>
+    </div>
+  )
 }
 
 const Sisalto = (props) => {
-    return (
-        <div>
-            <Osa nimi={props.osat[0].nimi} tehtavia={props.osat[0].tehtavia}/>
-            <Osa nimi={props.osat[1].nimi} tehtavia={props.osat[1].tehtavia}/>
-            <Osa nimi={props.osat[2].nimi} tehtavia={props.osat[2].tehtavia}/>
-        </div>
-    )
+  return (
+    <div>
+      <Osa nimi={props.osat[0].nimi} tehtavia={props.osat[0].tehtavia}/>
+      <Osa nimi={props.osat[1].nimi} tehtavia={props.osat[1].tehtavia}/>
+      <Osa nimi={props.osat[2].nimi} tehtavia={props.osat[2].tehtavia}/>
+    </div>
+  )
 }
 
 const Yhteensa = (props) => {
-    return (
-        <div>
-            <p>yhteensä {props.osat[0].tehtavia +
-                props.osat[1].tehtavia +
-                props.osat[2].tehtavia} tehtävää</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>yhteensä {props.osat[0].tehtavia +
+        props.osat[1].tehtavia +
+        props.osat[2].tehtavia} tehtävää
+      </p>
+    </div>
+  )
 }
 
 const App = () => {
   const kurssi = {
-      nimi: 'Half Stack -sovelluskehitys',
-      osat: [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14
-        }
-      ]
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
   }
 
   return (
