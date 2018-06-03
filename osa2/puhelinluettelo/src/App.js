5adf6772f92a942a2335d0c50e777f669605ba97
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-06-03 10:54:38 +0300

    Add code for 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -17,10 +17,14 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas', number: '040 123456' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
       ],
       newName: '',
-      newNumber: ''
+      newNumber: '',
+      filter: ''
     }
   }
 
@@ -59,10 +63,18 @@ class App extends React.Component {
     this.setState({ newNumber: event.target.value })
   }
 
+  handleFilterChange = (event) => {
+    this.setState({ filter: event.target.value })
+  }
+
   render() {
     return (
       <div>
-        <h2>Puhelinluettelo</h2>
+        <h1>Puhelinluettelo</h1>
+        <div>
+          rajaa näytettäviä: <input value={this.state.filter} onChange={this.handleFilterChange}/>
+        </div>
+        <h2>Lisää uusi</h2>
         <form onSubmit={this.addPerson}>
           <div>
             nimi: <input value={this.state.newName} onChange={this.handleNameChange}/>
@@ -77,10 +89,11 @@ class App extends React.Component {
         <h2>Numerot</h2>
         <table>
           <tbody>
-            {this.state.persons.map(person => <PersonListItem
-                                              key={person.name}
-                                              name={person.name}
-                                              number={person.number}/>)}
+            {this.state.persons.filter(
+              person => { return person.name.toLowerCase().includes(this.state.filter.toLowerCase()) }
+            ).map(
+              person => <PersonListItem key={person.name} name={person.name} number={person.number}/>)
+            }
           </tbody>
         </table>
       </div>
