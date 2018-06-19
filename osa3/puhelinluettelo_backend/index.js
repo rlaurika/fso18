a42e3c8b6342cd74755f562adeca9cd5837ae03e
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-06-19 20:42:42 +0300

    Add code for 3.1
    Create a basic backend for the phonebook application that serves a hard-coded
    list of persons for consumption by the phonebook application at the
    /api/persons API endpoint.

---
+++ osa3/puhelinluettelo_backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Martti Tienari",
+    "number": "040-123456",
+    "id": 2
+  },
+  {
+    "name": "Arto Järvinen",
+    "number": "040-123456",
+    "id": 3
+  },
+  {
+    "name": "Lea Kutvonen",
+    "number": "040-123456",
+    "id": 4
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  console.log('Request from the following agent: ', request.get('User-Agent'))
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
