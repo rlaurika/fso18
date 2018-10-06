362587f011593440438439c4a27cb2534cfe44b5
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-10-06 15:51:44 +0300

    Add code for 3.16
    Phonebook:
    
    Make it so that the delete operation deletes persons from the database.

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -104,10 +104,14 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id'})
+    })
 })
 
 const PORT = process.env.PORT || 3001
