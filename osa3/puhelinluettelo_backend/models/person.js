7dbfc1b0cf93a668d3f0f57bff86aedab1eb344f
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-10-07 15:37:29 +0300

    Use dotenv for phonebook backend config
    Install the dotenv library to use .env files for configuring the phonebook
    backend when NODE_ENV is not set to "production".

--- osa3/puhelinluettelo_backend/models/person.js
+++ osa3/puhelinluettelo_backend/models/person.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
 const mongo_user = process.env.MONGO_USER
 const mongo_pass = process.env.MONGO_PASS
 const mongo_url = process.env.MONGO_URL
 
 const url = 'mongodb://'+mongo_user+':'+mongo_pass+'@'+mongo_url
 
-mongoose.connect(url)
+mongoose.connect(url, { useNewUrlParser: true })
 
 const personSchema = new mongoose.Schema({
   name: String,
