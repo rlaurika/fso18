c518270d7cc3834b66e487a5402c694fee923f46
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-10-11 20:39:59 +0300

    4.4: Bloglist backend: totalLikes function + tests
    Add a function totalLikes that takes a list of blogs and sums up their likes.
    Add unit tests for this new function.

--- osa4/bloglist-backend/utils/list_helper.js
+++ osa4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sumOfLikes, item) => {
+    if (typeof(item.likes) !== 'undefined') {
+      return sumOfLikes + item.likes
+    } else {
+      throw 'Item in array has no likes field'
+    }
+  }
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
