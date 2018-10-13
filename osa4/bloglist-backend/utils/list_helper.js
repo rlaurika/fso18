7645717cf300fb221dae3ef08ee0c6cb5303cfbb
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-10-13 14:01:45 +0300

    4.5: Bloglist backend: favoriteBlog function+tests
    Add a new function favoriteBlog that takes an array of blogs and determines
    which one has the most likes. Add tests for this function. Move common test
    data out of a describe block so that it is available in all describe blocks.
    
    Fix a couple of lint issues.

--- osa4/bloglist-backend/utils/list_helper.js
+++ osa4/bloglist-backend/utils/list_helper.js
@@ -1,11 +1,12 @@
 const dummy = (blogs) => {
-  return 1
+  blogs = 1
+  return blogs
 }
 
 const totalLikes = (blogs) => {
-  const reducer = (sumOfLikes, item) => {
-    if (typeof(item.likes) !== 'undefined') {
-      return sumOfLikes + item.likes
+  const reducer = (sumOfLikes, blog) => {
+    if (typeof(blog.likes) !== 'undefined') {
+      return sumOfLikes + blog.likes
     } else {
       throw 'Item in array has no likes field'
     }
@@ -14,7 +15,24 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (favoriteBlog, blog) => {
+    if (typeof(blog.likes) !== 'undefined') {
+      blog = {
+        'author': blog.author,
+        'likes': blog.likes,
+        'title': blog.title
+      }
+
+      return blog.likes > favoriteBlog.likes ? blog : favoriteBlog
+    }
+  }
+
+  return blogs.length === 0 ? undefined : blogs.reduce(reducer, { likes: 0 })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
