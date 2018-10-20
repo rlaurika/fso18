9bd874c6b8867879f156843b0787e41507213cdf
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-10-20 16:15:42 +0300

    4.12: Bloglist backend: refactor tests
    - Add a new test_util file that contains initial test data and a helper
      function for retrieving blogs directly from the DB
    - Use the new helper function in the tests for the bloglist backend so that we
      don't have to make additional GET calls to /api/blogs for getting a list of
      all blogs for comparisons

--- osa4/bloglist-backend/tests/blog_api.test.js
+++ osa4/bloglist-backend/tests/blog_api.test.js
@@ -2,57 +2,7 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+const { initialBlogs, blogsInDb } = require('./test_util')
 
 beforeAll(async () => {
   await Blog.deleteMany({})
@@ -72,8 +22,9 @@ describe('get all blogs', () => {
 
   test('returns the right number of blogs', async () => {
     const response = await api.get('/api/blogs')
+    const blogs = await blogsInDb()
 
-    expect(response.body.length).toBe(initialBlogs.length)
+    expect(response.body.length).toBe(blogs.length)
   })
 })
 
@@ -92,9 +43,9 @@ describe('add new blog', () => {
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const allBlogs = await api.get('/api/blogs')
+    const allBlogs = await blogsInDb()
 
-    const titles = allBlogs.body.map(blog => blog.title)
+    const titles = allBlogs.map(blog => blog.title)
 
     expect(titles).toContain('Why Software Is Eating the World')
   })
@@ -107,7 +58,7 @@ describe('add new blog', () => {
       'likes': 0
     }
 
-    const blogsBeforeAdd = await api.get('/api/blogs')
+    const blogsBeforeAdd = await blogsInDb()
 
     await api
       .post('/api/blogs')
@@ -115,12 +66,9 @@ describe('add new blog', () => {
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const blogsAfterAdd = await api.get('/api/blogs')
+    const blogsAfterAdd = await blogsInDb()
 
-    const blogTitles = blogsAfterAdd.body.map(blog => blog.title)
-
-    expect(blogTitles).toContain('A Cherry Picker\'s Guide to Doctor Who')
-    expect(blogsAfterAdd.body.length).toBe(blogsBeforeAdd.body.length + 1)
+    expect(blogsAfterAdd.length).toBe(blogsBeforeAdd.length + 1)
   })
 
   test('adds the likes field if it is missing', async () => {
