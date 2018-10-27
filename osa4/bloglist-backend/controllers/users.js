0aa106e3dd78030c79a4847a6fcb598895f6e908
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-10-27 16:27:25 +0300

    4.16: Bloglist backend: user checks+tests
    - Ensure password length for new users is at least 3 chars
    - Set adult to true by default when creating new users
    - Ensure user accounts created are unique
    - Add tests for the three functionalities above
    - Add tests in general for user creation
    - Add helpers to test_util.js for user API testing
    - Don't bind to a port when running tests - this prevents conflicts where a
      port is already bound

--- osa4/bloglist-backend/controllers/users.js
+++ osa4/bloglist-backend/controllers/users.js
@@ -13,19 +13,36 @@ usersRouter.post('/', async (request, response) => {
   try {
     const body = request.body
 
+    if (body.password && body.password.length < 3) {
+      return response.status(400).json(
+        { error: 'password given is too short (must be at least 3 chars long)' }
+      )
+    }
+
+    const existingUser = await User.find({ username: body.username })
+
+    if (existingUser.length !== 0) {
+      return response.status(409).json({ error: 'a user with that username already exists' })
+    }
+
+    let adult = true
+    if (body.adult !== undefined) {
+      adult = body.adult
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
     const user = new User({
       username: body.username,
       name: body.name,
-      adult: body.adult,
+      adult: adult,
       passwordHash
     })
 
     const savedUser = await user.save()
 
-    response.json(User.format(savedUser))
+    response.status(201).json(User.format(savedUser))
   } catch (exception) {
     response.status(500).json({ error: 'something went wrong' })
   }
