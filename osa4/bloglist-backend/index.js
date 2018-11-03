8bf494ed080a33e2c14d000318b5fa82bf353889
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-11-03 14:54:17 +0200

    4.18: Blocklist backend: add user login
    Make it possible for users to login via a new login API.

--- osa4/bloglist-backend/index.js
+++ osa4/bloglist-backend/index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose
   .connect(config.mongoUrl, { useNewUrlParser: true })
@@ -22,6 +23,7 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const server = http.createServer(app)
 
