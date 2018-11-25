675a8efb5a80558422f147a54e3bf68d0c6ab929
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-11-25 15:29:03 +0200

    5.2: Bloglist frontend: local storage for logins
    Use browser local storage to store the token for the current session. Also add
    a log out functionality.

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -19,6 +19,13 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
+
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      this.setState({ user })
+      blogService.setToken(user.token)
+    }
   }
 
   doLogin = async (event) => {
@@ -31,9 +38,15 @@ class App extends React.Component {
 
       blogService.setToken(user.token)
       this.setState({ username: '', password: '', user })
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
     } catch (exception) {
       alert('invalid username or password')
     }
+  }
+
+  doLogout = () => {
+    window.localStorage.removeItem('loggedInUser')
+    this.setState({ user: null })
   }
 
   handleLoginFieldChange = (event) => {
@@ -50,7 +63,10 @@ class App extends React.Component {
           />
         </div> :
         <div>
-          <p>{this.state.user.name} logged in</p>
+          <div>
+            <p>{this.state.user.name} logged in</p>
+            <button onClick={this.doLogout}>log out</button>
+          </div>
           <BlogList blogs={ this.state.blogs }/>
         </div>
       )
