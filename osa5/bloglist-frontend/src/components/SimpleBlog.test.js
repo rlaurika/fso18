b9e3cc841f1a647229d6b5b7b82bd0c31d1a8d70
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-12-01 19:11:19 +0200

    5.12: Bloglist frontend: SimpleBlog test
    Make some tests for a SimpleBlog component:
    
    - Test that title and author are rendered
    - Test that number of likes is rendered
    
    Use Enzyme and Jest. Install Enzyme and add a setupTests.js file to initialize
    it.

---
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import SimpleBlog from './SimpleBlog'
+
+describe.only('<SimpleBlog />', () => {
+  let simpleBlogComponent
+  let blog
+  let mockClickHandler
+
+  beforeEach(() => {
+    blog = {
+      title: 'There is no longer any such thing as Computer Security',
+      author: 'Jeff Atwood',
+      likes: 6
+    }
+
+    mockClickHandler = jest.fn()
+
+    simpleBlogComponent = shallow(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockClickHandler}
+      />
+    )
+  })
+
+  it('renders title and author', () => {
+    const titleAndAuthorDiv = simpleBlogComponent.find('.title_and_author')
+    expect(titleAndAuthorDiv.text()).toContain(`${blog.title} ${blog.author}`)
+  })
+
+  it('renders likes', () => {
+    const likesDiv = simpleBlogComponent.find('.likes')
+    expect(likesDiv.text()).toContain(`blog has ${blog.likes} likes`)
+  })
+})
