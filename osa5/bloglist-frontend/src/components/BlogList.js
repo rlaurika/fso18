2bbf7ba01583b037e074268652edb447b97358ef
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-12-01 12:13:52 +0200

    5.8: Bloglist frontend: sort the bloglist by likes
    Sort the blogs in the list in descending order by number of likes.

--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Blog from './Blog'
 
-const BlogList = ({blogs, likeButtonHandler}) => (
-  <div>
-    <h2>blogs</h2>
-    {blogs.map(blog =>
-      <Blog
-        key={blog._id}
-        blog={blog}
-        likeButtonHandler={likeButtonHandler(blog._id)}/>
-    )}
-  </div>
-)
+const BlogList = ({blogs, likeButtonHandler}) => {
+  const sortedBlogs = blogs.sort((a, b) => {
+    return b.likes - a.likes
+  })
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {sortedBlogs.map(blog =>
+        <Blog
+          key={blog._id}
+          blog={blog}
+          likeButtonHandler={likeButtonHandler(blog._id)}
+        />
+      )}
+    </div>
+  )
+}
 
 export default BlogList
