81a5dc1fe7fe90790a8687051786dbb947c0da4e
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-12-07 18:29:22 +0200

    6.2: Redux-anecdotes-v2: action creators
    Use action creators for voting and anecdote creation instead of direct
    references to actions.

--- osa6/redux-anecdotes-v2/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes-v2/src/reducers/anecdoteReducer.js
@@ -34,4 +34,18 @@ const reducer = (store = initialState, action) => {
   return store
 }
 
+export const voting = (id) => {
+  return {
+    type: 'VOTE',
+    id
+  }
+}
+
+export const anecdoteCreation = (content) => {
+  return {
+    type: 'CREATE',
+    content
+  }
+}
+
 export default reducer
