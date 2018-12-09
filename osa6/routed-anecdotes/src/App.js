c0ee6e22ddaf8262e58962c4344260cbafbe0589
Risto Laurikainen <risto.laurikainen@iki.fi>
2018-12-09 18:53:47 +0200

    6.16: Routed-anecdotes: setup React Router
    Use React Router to switch between views when clicking links in the top menu
    bar. Use the following paths:
    
    - /
    - /create
    - /about

--- osa6/routed-anecdotes/src/App.js
+++ osa6/routed-anecdotes/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const Menu = () => (
   <div>    
-    <a href='#'>anecdotes</a>&nbsp;
-    <a href='#'>create new</a>&nbsp;
-    <a href='#'>about</a>&nbsp;
+    <Link to='/'>anecdotes</Link>&nbsp;
+    <Link to='/create'>create new</Link>&nbsp;
+    <Link to='/about'>about</Link>&nbsp;
   </div>
 )
 
@@ -138,12 +139,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Software anecdotes</h1>
-          <Menu />
-          <AnecdoteList anecdotes={this.state.anecdotes} />
-          <About />      
-          <CreateNew addNew={this.addNew}/>
-        <Footer />
+        <Router>
+          <div>
+            <h1>Software anecdotes</h1>
+              <Menu />
+              <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
+              <Route exact path="/about" render={() => <About />} />
+              <Route exact path="/create" render={() => <CreateNew addNew={this.addNew}/>} />
+            <Footer />
+          </div>
+        </Router>
       </div>
     );
   }
